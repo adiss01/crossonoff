32ff006ec3d014520073f1f03efea233b28e1ec3
adiss01 <69195521+adiss01@users.noreply.github.com>
2020-12-06 03:04:32 +0300

    Create cl.lua

---
+++ cl.lua
@@ -0,0 +1,26 @@
+local cross = false
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1)
+        if IsControlJustPressed(0, 166) then 
+            if cross then
+                exports['mythic_notify']:SendAlert('inform', 'cross açıldı')
+                cross = false
+            elseif not cross then 
+                exports['mythic_notify']:SendAlert('inform', 'cross kapandı')
+                cross = true
+            end
+        end
+        if cross then 
+            HideHudComponentThisFrame(14)
+        elseif not cross then 
+            local ped = GetPlayerPed(-1)
+            local weapon = GetSelectedPedWeapon(ped)
+    
+            if weapon == GetHashKey("WEAPON_") then       
+                ShowHudComponentThisFrame(14)
+            end
+        end
+    end
+end)
